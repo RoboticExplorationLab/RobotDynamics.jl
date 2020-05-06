874227ae622b93ce85753930a33b4c3770ad05ef
Brian Jackson <bjack205@gmail.com>
2020-05-05 17:42:03 -0700

    Update deps and tests

--- test/liestate.jl
+++ test/liestate.jl
@@ -1,9 +1,11 @@
-
+using Test
 using StaticArrays
 using Rotations
 using LinearAlgebra
 using BenchmarkTools
 using RobotDynamics
+
+import RobotDynamics: LieState
 
 function error_state_quat(x::AbstractVector, x0::AbstractVector)
     dq1 = UnitQuaternion(x[4],x[5],x[6],x[7]) ⊖ UnitQuaternion(x0[4],x0[5],x0[6],x0[7])
@@ -24,50 +26,50 @@ end
 R = UnitQuaternion{Float64}
 P = (3,2,3)
 s = LieState{R,P}()
-length(s) == 16
-error_size(s) == 14
-vec_inds(R,P,1) == 1:3   == inds(R,P,1)
-rot_inds(R,P,1) == 4:7   == inds(R,P,2)
-vec_inds(R,P,2) == 8:9   == inds(R,P,3)
-rot_inds(R,P,2) == 10:13 == inds(R,P,4)
-vec_inds(R,P,3) == 14:16 == inds(R,P,5)
+@test length(s) == 16
+@test state_diff_size(s) == 14
+@test RobotDynamics.vec_inds(R,P,1) == 1:3   == RobotDynamics.inds(R,P,1)
+@test RobotDynamics.rot_inds(R,P,1) == 4:7   == RobotDynamics.inds(R,P,2)
+@test RobotDynamics.vec_inds(R,P,2) == 8:9   == RobotDynamics.inds(R,P,3)
+@test RobotDynamics.rot_inds(R,P,2) == 10:13 == RobotDynamics.inds(R,P,4)
+@test RobotDynamics.vec_inds(R,P,3) == 14:16 == RobotDynamics.inds(R,P,5)
 
 n = length(s)
 x  = @MVector rand(n)
 x0 = @MVector rand(n)
-state_diff(s, x, x0) ≈ error_state_quat(x, x0)
+@test RobotDynamics.state_diff(s, x, x0) ≈ error_state_quat(x, x0)
 
 # [1,2,3], (4,5,6), [7,8], (9,10,11), [12,13,14]
 R = RodriguesParam{Float64}
 P = (3,2,3)
 s = LieState{R,P}()
-length(s) == 14
-error_size(s) == 14
-vec_inds(R,P,1) == 1:3   == inds(R,P,1)
-rot_inds(R,P,1) == 4:6   == inds(R,P,2)
-vec_inds(R,P,2) == 7:8   == inds(R,P,3)
-rot_inds(R,P,2) == 9:11  == inds(R,P,4)
-vec_inds(R,P,3) == 12:14 == inds(R,P,5)
-error_state(s, x, x0) ≈ error_state_rp(x, x0)
+@test length(s) == 14
+@test state_diff_size(s) == 14
+@test RobotDynamics.vec_inds(R,P,1) == 1:3   == RobotDynamics.inds(R,P,1)
+@test RobotDynamics.rot_inds(R,P,1) == 4:6   == RobotDynamics.inds(R,P,2)
+@test RobotDynamics.vec_inds(R,P,2) == 7:8   == RobotDynamics.inds(R,P,3)
+@test RobotDynamics.rot_inds(R,P,2) == 9:11  == RobotDynamics.inds(R,P,4)
+@test RobotDynamics.vec_inds(R,P,3) == 12:14 == RobotDynamics.inds(R,P,5)
+@test RobotDynamics.state_diff(s, x, x0) ≈ error_state_rp(x, x0)
 
-@btime error_state($s, $x, $x0)
-@btime error_state_rp($x, $x0)
+# @btime error_state($s, $x, $x0)
+# @btime error_state_rp($x, $x0)
 
 # state diff jacobian
 R = UnitQuaternion{Float64}
 P = (3,2,3)
 s = LieState{R,P}()
 n = length(s)
-n̄ = error_size(s)
+n̄ = state_diff_size(s)
 G = zeros(n,n̄)
-state_diff_jacobian!(G, s, x)
+RobotDynamics.state_diff_jacobian!(G, s, x)
 
 q1 = UnitQuaternion(x[4],x[5],x[6],x[7])
 q2 = UnitQuaternion(x[10],x[11],x[12],x[13])
 G1 = Rotations.∇differential(q1)
 G2 = Rotations.∇differential(q2)
 G0 = cat(I(3),G1,I(2),G2,I(3),dims=(1,2))
-G0 ≈ G
+@test G0 ≈ G
 
 # state diff jacobian jacobian
 ∇G = SizedMatrix{n̄,n̄}(zeros(n̄,n̄))
@@ -79,4 +81,4 @@ RobotDynamics.∇²differential!(∇G, s, x, dx)
 ∇G1 = Rotations.∇²differential(q1, dq1)
 ∇G2 = Rotations.∇²differential(q2, dq2)
 G0 = cat(zeros(3,3), ∇G1, zeros(2,2), ∇G2, zeros(3,3), dims=(1,2))
-G0 ≈ ∇G
+@test G0 ≈ ∇G
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,3 +5,7 @@ using StaticArrays
 @testset "Basic Dynamics" begin
     include("cartpole_test.jl")
 end
+
+@testset "Lie State" begin
+    include("liestate.jl")
+end
